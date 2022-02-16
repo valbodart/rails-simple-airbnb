59a41d2be99f4149dd903620f80f96127d2b2a79
Valentine Bodart <valentine.bodart@gmail.com>
2022-02-16 15:49:32 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+Flat.create!(
+  name: "Nice #{%w[flat mansion house appartment cabin castle].sample} in #{Faker::Address.city}",
+  address: Faker::Address.full_address,
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: rand(50..175),
+  number_of_guests: rand(1..7)
+)
+end
